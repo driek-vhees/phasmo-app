dbce38373991a87fa1161b96d64ed1088c3157a9
Driek <45156412+driek-vhees@users.noreply.github.com>
2021-06-20 18:35:04 +0900

    Remove part of sample app

--- src/App.tsx
+++ src/App.tsx
@@ -36,14 +36,6 @@ const EvidenceButton = ({evidence: { evidenceName, evidenceType }, onAdd, onRemo
 }
 
 function App({}: AppProps) {
-  // Create the count state.
-  const [count, setCount] = useState(0);
-  // Create the counter (+1 every second).
-  useEffect(() => {
-    const timer = setTimeout(() => setCount(count + 1), 1000);
-    return () => clearTimeout(timer);
-  }, [count, setCount]);
-
   const [positiveEvidences, setPositiveEvidences] = useState<ReadonlyArray<EvidenceType>>([]);
 
   const addPositiveType = (evidenceType: EvidenceType) => {
